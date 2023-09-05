92d5245a87425650d07fb83cc8616049d6f117ee
Fabio Paredes <fabio.paredes@gmail.com>
2023-09-05 06:53:40 -0300

    End main section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graphic1Component } from './pages/graphic1/graphic1.component';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LoginComponent,
+        RegisterComponent,
+        NopagefoundComponent,
+        DashboardComponent,
+        BreadcrumbsComponent,
+        SidebarComponent,
+        HeaderComponent,
+        FooterComponent,
+        ProgressComponent,
+        Graphic1Component,
+        PagesComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
