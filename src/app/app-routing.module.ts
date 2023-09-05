21f2a031bd1f6e567983882aeedf3ca328e92450
Fabio Paredes <fabio.paredes@gmail.com>
2023-09-05 17:13:18 -0300

    Routing pages and auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,37 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes  } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graphic1Component } from './pages/graphic1/graphic1.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
+
+// Modules
+import { PagesRoutingModule } from './pages/pages-routing';
+import { AuthRoutingModule } from './auth/auth.routing';
+
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 const routes: Routes = [
 
-    {
-        path: '',
-        component: PagesComponent,
-        children:[
-            { path: 'dashboard', component: DashboardComponent},
-            { path: 'progress', component: ProgressComponent},
-            { path: 'graphic1', component: Graphic1Component},
-            { path: '' , redirectTo: '/dashboard', pathMatch: 'full'},
-        ]
-    },
-
-    { path: 'register', component: RegisterComponent},
-    { path: 'login', component: LoginComponent},
-
+    // { path: '/dashboard' , component: PagesRoutingModule},
+    // { path: '/auth' , component: AuthRoutingModule},
+    // { path: '/doctors' , component: DoctorRoutingModule},
+    // { path: '/purchase' , component: PurchaseRoutingModule},
+    { path: '' , redirectTo: 'dashboard', pathMatch: 'full' },
     { path: '**' , component: NopagefoundComponent},
 ];
 
 
 @NgModule({
-    declarations: [],
     imports: [
-        RouterModule.forRoot( routes )
+        RouterModule.forRoot( routes ),
+        PagesRoutingModule,
+        AuthRoutingModule
     ],
     exports: [
         RouterModule
